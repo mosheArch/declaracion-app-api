dc6224d102b68ac104339955a987774a005742c3
mosheArch <alvaro.moises.diaz.diaz@gmail.com>
2020-08-22 00:36:28 -0500

    Configuracion del proyecto

---
+++ app/app/settings/local.py
@@ -0,0 +1,33 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR.child('db.sqlite3'),
+    }
+}
+
+# REST_FRAMEWORK={
+#     'DEFAULT_PERMISSION_CLASSES': {
+#         'rest_framework.permissions.IsAuthenticated'
+#     }
+# }
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
